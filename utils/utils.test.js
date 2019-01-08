95ec06b41b546fd7b37e121474772b8e5d267a88
Cody Reed <cody2j2reed@gmail.com>
2019-01-08 14:16:04 -0500

    Update utils.test.js

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,19 @@
+const expect = require("expect");
+
 const utils = require("./utils");
 
 it("should add two numbers", () => {
   let res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`);
-  }
+
+  expect(res)
+    .toBe(44)
+    .toBeA("number");
 });
 
 it("should multiply one number by itself", () => {
   let res = utils.square(3);
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`);
-  }
+
+  expect(res)
+    .toBe(9)
+    .toBeA("number");
 });
