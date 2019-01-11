e8fc62c14ccbb1a2bcd8f85e9219c5d1e3a88725
Cody Reed <cody2j2reed@gmail.com>
2019-01-11 09:13:06 -0500

    Update utils.test.js

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,63 +2,52 @@ const expect = require("expect");
 
 const utils = require("./utils");
 
-it("should add two numbers", () => {
-  let res = utils.add(33, 11);
+describe("Utils", () => {
+  it("should add two numbers", () => {
+    let res = utils.add(33, 11);
 
-  expect(res)
-    .toBe(44)
-    .toBeA("number");
-});
-
-it("should aysnc add two numbers", done => {
-  utils.asyncAdd(3, 4, sum => {
-    expect(sum)
-      .toBe(7)
+    expect(res)
+      .toBe(44)
       .toBeA("number");
-    done();
   });
-});
 
-it("should multiply one number by itself", () => {
-  let res = utils.square(3);
+  it("should aysnc add two numbers", done => {
+    utils.asyncAdd(3, 4, sum => {
+      expect(sum)
+        .toBe(7)
+        .toBeA("number");
+      done();
+    });
+  });
 
-  expect(res)
-    .toBe(9)
-    .toBeA("number");
-});
+  it("should multiply one number by itself", () => {
+    let res = utils.square(3);
 
-it("should aysnc square a number", done => {
-  utils.asyncSquare(3, sum => {
-    expect(sum)
+    expect(res)
       .toBe(9)
       .toBeA("number");
-    done();
   });
-});
 
-// it("should expect some values", () => {
-//   // expect(12).toNotBe(11);
-//   // expect({ name: "cody" }).toNotEqual({ name: "Cody" });
-//   // expect([2, 3, 4]).toExclude(1);
-//   expect({
-//     name: "Cody",
-//     age: 33,
-//     location: "Knoxville"
-//   }).toExclude({
-//     age: 32
-//   });
-// });
+  it("should aysnc square a number", done => {
+    utils.asyncSquare(3, sum => {
+      expect(sum)
+        .toBe(9)
+        .toBeA("number");
+      done();
+    });
+  });
 
-it("should verify first and last names are set", () => {
-  let user = {
-    location: "Knoxville",
-    age: 33
-  };
+  it("should verify first and last names are set", () => {
+    let user = {
+      location: "Knoxville",
+      age: 33
+    };
 
-  utils.setName(user, "Cody Reed");
+    utils.setName(user, "Cody Reed");
 
-  expect(user).toInclude({
-    firstName: "Cody",
-    lastName: "Reed"
+    expect(user).toInclude({
+      firstName: "Cody",
+      lastName: "Reed"
+    });
   });
 });
