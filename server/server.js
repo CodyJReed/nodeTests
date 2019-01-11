491aca433d90e69bccc703bd253c6b559bdc111c
Cody Reed <cody2j2reed@gmail.com>
2019-01-11 08:54:51 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,23 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/users", (req, res) => {
+  res.status(200).send([
+    {
+      name: "Cody",
+      age: 33
+    },
+    {
+      name: "Scarlett",
+      age: 44
+    },
+    {
+      name: "Indigo",
+      age: 1
+    }
+  ]);
+});
+
 app.listen(3000);
 
 module.exports.app = app;
