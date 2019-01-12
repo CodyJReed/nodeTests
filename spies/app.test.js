71bbbd814b83f18984bab2a121340dafde7c3e38
Cody Reed <cody2j2reed@gmail.com>
2019-01-12 17:37:52 -0500

    Update app.test.js

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,10 +1,26 @@
 const expect = require("expect");
+const rewire = require("rewire");
+
+const app = rewire("./app");
 
 describe("App", () => {
+  const db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__("db", db);
+
   it("should call to spy correctly", () => {
     const spy = expect.createSpy();
     spy("Cody", 33);
     // Checks for spy() truthy
     expect(spy).toHaveBeenCalledWith("Cody", 33);
+  });
+
+  it("should call saveUser with user object", () => {
+    const email = "test@example.com";
+    const password = "test123";
+
+    app.handleSignup(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({ email, password });
   });
 });
